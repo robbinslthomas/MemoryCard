f2b66f852e04fce5af7f5f59f2b716a739350a06
Thomas Robbins <robbinslthomas@gmail.com>
2018-02-05 12:18:49 +0000

    Added Javascript, elementList still not creating properly

--- scripts/mainScript.js
+++ scripts/mainScript.js
@@ -1,2 +1,4 @@
-var list = document.querySelectorAll( '.card' );
-console.log(list.length);
+  document.addEventListener("load", function(event){
+  console.log("This part is working");
+  var elementList = document.querySelectorAll("card");
+}, true);
